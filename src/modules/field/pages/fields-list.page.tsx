d93259fb9ec259ae4ad16bb432d5f677f66cf1e5
MicaelRiboura <micaelribouradossantos@gmail.com>
2024-06-25 19:07:35 -0300

    feat: create page fields list

---
+++ src/modules/field/pages/fields-list.page.tsx
@@ -0,0 +1,156 @@
+import { Button } from "../../../components/ui/button";
+import { Payment, columns } from "../components/field-table/columns";
+import { DataTable } from "../components/field-table/data-table";
+
+export function FieldsList() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">Talhões</h1>
+            <div className="flex items-center justify-end my-3">
+                <Button>Registrar Novo</Button>
+            </div>
+            <div className="w-full">
+                <DataTable columns={columns} data={[
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                    {
+                        id: "728ed52f",
+                        amount: 100,
+                        status: "pending",
+                        email: "m@example.com",
+                    } as Payment,
+                ]} />
+            </div>
+        </div>
+    )
+}
